b9cdded55d9d5b5226d138d889fa73cae7ef2aa7
eslam <eslamwaelsaeid@gmail.com>
2025-02-16 18:09:26 +0200

    errors vercal resolve

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // تأكد من أنه موجود لمنع مشاكل المسارات
   build: {
     rollupOptions: {
-      external: ['@react-aria/focus']
     }
   }
 })
