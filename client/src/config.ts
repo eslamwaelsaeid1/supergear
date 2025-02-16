cf7f6effdbfc69de12ed3da8a5b49127b7367e8c
eslam <eslamwaelsaeid@gmail.com>
2025-02-16 06:28:13 +0200

    Remove .env and node_modules from repository

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,7 +6,7 @@ const checkConfig = (server: string): Config | object => {
   switch (server) {
     case "production":
       config = {
-        baseUrl: "",
+        baseUrl: "https://supergear-opal.vercel.app",
       };
       break;
     case "local":
